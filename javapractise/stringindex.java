57ff30bf9f2aff6abdb18eecadcacd765c030b63
Jokestir <bugs.buny01@yandex.com>
2015-11-16 22:21:42 +0530

    indexOf

---
+++ javapractise/stringindex.java
@@ -0,0 +1,27 @@
+public class stringindex{
+
+	public static void main(String[] args){
+
+
+		String str1 = "Let us know each other";
+
+		String str2 = "know";
+
+		String str3 = "what";
+
+
+		System.out.println(str1.indexOf('o'));
+		System.out.println(str1.indexOf('o',10));
+		System.out.println(str1.indexOf(str2));
+		System.out.println(str1.indexOf(str2,8));
+
+
+
+
+
+
+	}
+
+
+
+}
