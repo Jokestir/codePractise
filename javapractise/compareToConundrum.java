ebad1e2771775e44e594a8684e45749f60f57a61
Jokestir <bugs.buny01@yandex.com>
2015-11-19 23:47:34 +0530

    compareTo

---
+++ javapractise/compareToConundrum.java
@@ -0,0 +1,21 @@
+public class compareToConundrum{
+
+	public static void main(String[] args){
+
+		String str1 = "abc";
+
+		String str2 = "abcd";
+
+		System.out.printf(str1.compareTo(str2));
+
+
+
+
+
+	}
+
+
+
+
+
+}
