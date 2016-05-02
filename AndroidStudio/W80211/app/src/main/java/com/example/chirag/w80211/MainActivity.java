96c476d9d25457c7e3344d70ce46c1c6ed3dc161
Jokestir <bugs.buny01@yandex.com>
2016-05-02 12:37:24 +0530

    80211 ui changes

--- AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
+++ AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.chirag.w80211;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button getChannelListener = (Button ) findViewById(R.id.getChannelButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    getCh
+
+
+
 }
