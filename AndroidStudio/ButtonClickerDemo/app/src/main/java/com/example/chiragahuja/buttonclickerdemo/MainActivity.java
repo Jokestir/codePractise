fc9f28de29adbf8cbd357751a385f65f4fdbf7eb
Jokestir <bugs.buny01@yandex.com>
2016-05-02 17:50:50 +0530

    Project complete
    1. create button layout and button object
    
    2. create onclicklistener object
    
    3. assign button to onclicklistener object to the button using
    setOnclicklistener

--- AndroidStudio/ButtonClickerDemo/app/src/main/java/com/example/chiragahuja/buttonclickerdemo/MainActivity.java
+++ AndroidStudio/ButtonClickerDemo/app/src/main/java/com/example/chiragahuja/buttonclickerdemo/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.chiragahuja.buttonclickerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button okButton = (Button) findViewById(R.id.okButton);
+
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        final TextView textOutput = (TextView) findViewById(R.id.textOutput);
+
+        View.OnClickListener okBtnListenerObject = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                textOutput.setText("Ok presseed");
+
+            }
+        };
+
+
+        okButton.setOnClickListener(okBtnListenerObject);
+
+        View.OnClickListener cancelBtnOcl = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textOutput.setText("Cancel Button Pressed");
+            }
+        };
+
+
+        cancelButton.setOnClickListener(cancelBtnOcl);
     }
 }
