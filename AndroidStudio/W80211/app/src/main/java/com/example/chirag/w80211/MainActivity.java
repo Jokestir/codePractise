94c22a397f19c204f26b4163231615923888f9fd
Jokestir <bugs.buny01@yandex.com>
2016-05-02 19:00:15 +0530

    Added UI for getchannellist

--- AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
+++ AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.chirag.w80211;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiChannel;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button getChannelListener = (Button ) findViewById(R.id.getChannelButton);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button getChannel = (Button) findViewById(R.id.getChannelButton);
+        final TextView channelTitle = (TextView) findViewById(R.id.channelTitle);
+        final TextView channels = (TextView) findViewById(R.id.channelList);
+
+        View.OnClickListener oclGetChannelBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            channelTitle.setText(getString(R.string.channelTitle));
+                channels.setText(getString(R.string.channels));
+            }
+        };
+
+        getChannel.setOnClickListener(oclGetChannelBtn);
     }
 
 
