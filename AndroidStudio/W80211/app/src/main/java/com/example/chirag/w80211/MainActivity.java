267861b2dce76f0ec28c3324b543ce2c1b4d238d
Jokestir <bugs.buny01@yandex.com>
2016-05-03 19:43:08 +0530

    v 0.1.1
    segregate 2.4 an 5 ghz channels

--- AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
+++ AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chirag.w80211;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,8 @@ import android.content.Context;
 public class MainActivity extends Activity {
 
     WifiManager mWifiManger;
-
+    ArrayList<String> channels24ghzList;
+    ArrayList<String> channels5ghzList;
 
 
 
@@ -26,20 +28,34 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
+        channels24ghzList = new ArrayList<String>();
+        channels5ghzList = new ArrayList<String>();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWifiManger = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         Button getChannel = (Button) findViewById(R.id.getChannelButton);
-        final TextView channelTitle = (TextView) findViewById(R.id.channelTitle);
-        final TextView channels = (TextView) findViewById(R.id.channelList);
+        final TextView channel24ghzTitle = (TextView) findViewById(R.id.channel24ghzTitle);
+        final TextView channels24ghz = (TextView) findViewById(R.id.channels24ghzList);
+        final TextView channel5ghzTitle = (TextView) findViewById(R.id.channel5ghzTitle);
+        final TextView channels5ghz  = (TextView) findViewById(R.id.channels5ghzList);
 
         View.OnClickListener oclGetChannelBtn = new View.OnClickListener() {
-            String channelString;
+            //String channelString;
+
+
+
+
+
             @Override
             public void onClick(View view) {
 
-                // reset the string
-                channelString = "";
+                // reset the arrays
+
+                channels5ghzList.clear();
+                channels24ghzList.clear();
+
                 // reflection code
 
                 try {
@@ -62,19 +78,34 @@ public class MainActivity extends Activity {
 
                     ArrayList<?> listOfWifiChannelObjects = (ArrayList<?>) getChannelListMethod.invoke(mWifiManger,null);
 
-                    for (Object object : listOfWifiChannelObjects){
+                    for (Object wifiChannelObject : listOfWifiChannelObjects){
 
-                        Field channelNumField = object.getClass().getDeclaredField("channelNum");
+                        Field channelNumField = wifiChannelObject.getClass().getDeclaredField("channelNum");
 
-                        channelString = channelString.concat(channelNumField.get(object).toString());
+                        String channelNum = channelNumField.get(wifiChannelObject).toString();
 
 
-                        channelString = channelString.concat(", ");
+
+                        if(Integer.parseInt(channelNum) <= 14){
+
+                            channels24ghzList.add(channelNum);
+
+                        }
+
+                        else{
+
+                            channels5ghzList.add(channelNum);
+
+                        }
 
 
                     }
 
-                    channelString = channelString.substring(0,channelString.length()-2);
+                    String channels24ghz = TextUtils.join(", ", channels24ghzList);
+
+                    String channels5ghz = TextUtils.join(", ", channels5ghzList);
+
+                    //channelString = channelString.substring(0,channelString.length()-2);
 
                     //Log.e("CHIRAG"," " + getChannelListMethod.invoke(mWifiManger,null).getClass());
 
@@ -95,8 +126,11 @@ public class MainActivity extends Activity {
                 }
 
 
-            channelTitle.setText(getString(R.string.channelTitle));
-                channels.setText(channelString);
+                channel24ghzTitle.setText("2.4ghz channels");
+                channels24ghz.setText(android.text.TextUtils.join(", ", channels24ghzList));
+                channel5ghzTitle.setText("5 Ghz channels");
+                channels5ghz.setText(android.text.TextUtils.join(", ", channels5ghzList));
+
             }
         };
 
