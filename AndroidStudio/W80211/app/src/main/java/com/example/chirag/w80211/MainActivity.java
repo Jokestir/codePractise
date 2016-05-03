f614083ea4b85301e0c0b409fc61e9d5953d75d7
Jokestir <bugs.buny01@yandex.com>
2016-05-03 17:42:25 +0530

    v 0.1
    1. getChannelList() working properly.
    
    no async task or beautification

--- AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
+++ AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
@@ -1,32 +1,102 @@
 package com.example.chirag.w80211;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.net.wifi.WifiManager;
-import android.net.wifi.WifiChannel;
-import org.w3c.dom.Text;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+import android.net.wifi.WifiManager;
+import android.content.Context;
+
+
+public class MainActivity extends Activity {
+
+    WifiManager mWifiManger;
+
+
 
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWifiManger = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         Button getChannel = (Button) findViewById(R.id.getChannelButton);
         final TextView channelTitle = (TextView) findViewById(R.id.channelTitle);
         final TextView channels = (TextView) findViewById(R.id.channelList);
 
         View.OnClickListener oclGetChannelBtn = new View.OnClickListener() {
+            String channelString;
             @Override
             public void onClick(View view) {
+
+                // reset the string
+                channelString = "";
+                // reflection code
+
+                try {
+                    // load the class
+
+                    Class wifiChannelClass = Class.forName("android.net.wifi.WifiChannel");
+
+
+
+
+                    // create an instance of
+
+
+                   //Object WifiChannel = wifiChannelClass.newInstance();
+
+
+                    // find getChannelList() method from Wifimanager
+
+                    Method getChannelListMethod = WifiManager.class.getMethod("getChannelList",null);
+
+                    ArrayList<?> listOfWifiChannelObjects = (ArrayList<?>) getChannelListMethod.invoke(mWifiManger,null);
+
+                    for (Object object : listOfWifiChannelObjects){
+
+                        Field channelNumField = object.getClass().getDeclaredField("channelNum");
+
+                        channelString = channelString.concat(channelNumField.get(object).toString());
+
+
+                        channelString = channelString.concat(", ");
+
+
+                    }
+
+                    channelString = channelString.substring(0,channelString.length()-2);
+
+                    //Log.e("CHIRAG"," " + getChannelListMethod.invoke(mWifiManger,null).getClass());
+
+
+
+                    //channelString = wifichannels.toString();
+
+
+
+
+
+
+                }
+
+                catch(Exception exception){
+
+                    exception.printStackTrace();
+                }
+
+
             channelTitle.setText(getString(R.string.channelTitle));
-                channels.setText(getString(R.string.channels));
+                channels.setText(channelString);
             }
         };
 
