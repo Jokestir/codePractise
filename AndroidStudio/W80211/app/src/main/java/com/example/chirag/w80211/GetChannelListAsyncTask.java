be961844f63967fc4d37ccf17235c1d822d9adf2
Jokestir <bugs.buny01@yandex.com>
2016-05-10 23:30:54 +0530

    asyncTask version beta

--- AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/GetChannelListAsyncTask.java
+++ AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/GetChannelListAsyncTask.java
@@ -5,69 +5,94 @@ import android.os.AsyncTask;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
-/**
- * Created by chirag.ahuja on 5/9/2016.
- */
+
 public class GetChannelListAsyncTask extends AsyncTask{
 
 
-    // global app context declaration to access UI
+    /******* global objects in class
+    ****/
+
+
+    private static String TAG = "Beethoven";
     private Activity appContext;
 
-    // get wifimanager object
 
     WifiManager mWifiManager;
 
-    // channellists
     ArrayList<String> channels24ghzList;
     ArrayList<String> channels5ghzList;
 
-    // log tag
-    private static String TAG = "Beethoven";
+    TextView channel24ghzTitle;
+    TextView channels24ghz;
+    TextView channel5ghzTitle;
+    TextView channels5ghz;
 
 
-    // Over ride constructor to get activity context and initialize objects to be used in asynctask
+
+
+
 
     public GetChannelListAsyncTask(Context context) {
-
         super();
 
-        // // global app initialization to access UI
-
+        //get calling app
         this.appContext = (Activity) context;
 
-        // get wifimanager for reflection
 
         mWifiManager =  (WifiManager) this.appContext.getSystemService(Context.WIFI_SERVICE);
 
-        // initialize wifi channels arrayLists to empty everytime button is pressed.
+        //initialize UI
 
-        channels5ghzList.clear();
-        channels24ghzList.clear();
+        TextView channel24ghzTitle = (TextView) this.appContext.findViewById(R.id.channel24ghzTitle);
+        TextView channels24ghz = (TextView) this.appContext.findViewById(R.id.channels24ghzList);
+        TextView channel5ghzTitle = (TextView) this.appContext.findViewById(R.id.channel5ghzTitle);
+        TextView channels5ghz  = (TextView) this.appContext.findViewById(R.id.channels5ghzList);
 
 
-        Log.e(TAG,"Constructor AsyncTask");
     }
 
 
     @Override
     protected void onPreExecute() {
-
         super.onPreExecute();
 
-
-        Log.e(TAG,"OnPreExecute");
+        // clear channel list everytime
+        channels5ghzList.clear();
+        channels24ghzList.clear();
     }
 
     @Override
     protected Object doInBackground(Object[] objects) {
 
+        // reflection code to populate lists
+        try{
 
+            Method getChannelListMethod = WifiManager.class.getMethod("getChannelList");
+            ArrayList<?> listOfWifiChannelObjects = (ArrayList)getChannelListMethod.invoke(mWifiManager,null);
 
-        Log.e(TAG,"doInBackGround");
+            for (Object wifichannelObject : listOfWifiChannelObjects){
+
+                Field channelNumField = wifichannelObject.getClass().getField("channelNum");
+                String channelNum = channelNumField.get(wifichannelObject).toString();
+
+                if(Integer.parseInt(channelNum) <= 14){
+                    channels24ghzList.add(channelNum);
+                }
+                else{
+                    channels5ghzList.add(channelNum);
+                }
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
         return null;
     }
@@ -77,8 +102,11 @@ public class GetChannelListAsyncTask extends AsyncTask{
 
         super.onPostExecute(o);
 
-        Log.e(TAG,"OnPostExecute");
-
+        // update UX
+        channel24ghzTitle.setText("2.4ghz channels");
+        channels24ghz.setText(android.text.TextUtils.join(", ", channels24ghzList));
+        channel5ghzTitle.setText("5 Ghz channels");
+        channels5ghz.setText(android.text.TextUtils.join(", ", channels5ghzList));
     }
 }
 
--- AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
+++ AndroidStudio/W80211/app/src/main/java/com/example/chirag/w80211/MainActivity.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 import android.net.wifi.WifiManager;
 import android.content.Context;
+import com.example.chirag.w80211.GetChannelListAsyncTask;
 
 
 public class MainActivity extends Activity {
@@ -20,89 +21,30 @@ public class MainActivity extends Activity {
     ArrayList<String> channels24ghzList;
     ArrayList<String> channels5ghzList;
 
-
+    GetChannelListAsyncTask getChannelListAsyncTask;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         // create the content of the app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // channellists
-        channels24ghzList = new ArrayList<String>();
-        channels5ghzList = new ArrayList<String>();
-
-        // create UI objects
-        mWifiManger = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         Button getChannel = (Button) findViewById(R.id.getChannelButton);
-        final TextView channel24ghzTitle = (TextView) findViewById(R.id.channel24ghzTitle);
-        final TextView channels24ghz = (TextView) findViewById(R.id.channels24ghzList);
-        final TextView channel5ghzTitle = (TextView) findViewById(R.id.channel5ghzTitle);
-        final TextView channels5ghz  = (TextView) findViewById(R.id.channels5ghzList);
+
 
         View.OnClickListener oclGetChannelBtn = new View.OnClickListener() {
-            //String channelString;
-
-
-
-
 
             @Override
             public void onClick(View view) {
 
-                // reset the arrays
+                // execute asynctask to get channels and display
 
-                channels5ghzList.clear();
-                channels24ghzList.clear();
+                getChannelListAsyncTask = new GetChannelListAsyncTask(MainActivity.this);
 
-                // reflection code
-
-                try {
-
-
-                    Method getChannelListMethod = WifiManager.class.getMethod("getChannelList",null);
-
-                    ArrayList<?> listOfWifiChannelObjects = (ArrayList<?>) getChannelListMethod.invoke(mWifiManger,null);
-
-                    for (Object wifiChannelObject : listOfWifiChannelObjects){
-
-                        Field channelNumField = wifiChannelObject.getClass().getDeclaredField("channelNum");
-
-                        String channelNum = channelNumField.get(wifiChannelObject).toString();
-
-
-
-                        if(Integer.parseInt(channelNum) <= 14){
-
-                            channels24ghzList.add(channelNum);
-
-                        }
-
-                        else{
-
-                            channels5ghzList.add(channelNum);
-
-                        }
-
-
-                    }
-
-                }
-
-                catch(Exception exception){
-
-                    exception.printStackTrace();
-                }
-
-
-                channel24ghzTitle.setText("2.4ghz channels");
-                channels24ghz.setText(android.text.TextUtils.join(", ", channels24ghzList));
-                channel5ghzTitle.setText("5 Ghz channels");
-                channels5ghz.setText(android.text.TextUtils.join(", ", channels5ghzList));
+                getChannelListAsyncTask.execute();
 
             }
         };
