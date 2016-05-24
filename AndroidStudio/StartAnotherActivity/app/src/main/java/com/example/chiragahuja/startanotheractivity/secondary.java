b830b4f27236dd5c89beb6643499d44e0c35db1f
Jokestir <bugs.buny01@yandex.com>
2016-05-24 16:52:15 +0530

    Project finished

--- AndroidStudio/StartAnotherActivity/app/src/main/java/com/example/chiragahuja/startanotheractivity/secondary.java
+++ AndroidStudio/StartAnotherActivity/app/src/main/java/com/example/chiragahuja/startanotheractivity/secondary.java
@@ -1,7 +1,9 @@
 package com.example.chiragahuja.startanotheractivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class secondary extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class secondary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textOutput = (TextView) findViewById(R.id.textOutput);
+
+        if(textOutput!=null)
+            textOutput.setText(message);
     }
 }
